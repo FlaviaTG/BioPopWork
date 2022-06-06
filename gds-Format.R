304b95bab3e9c16aab2d4385e20f7074fe6108e6
FlaviaTG <57419069+FlaviaTG@users.noreply.github.com>
2022-06-06 12:55:29 -0400

    Update gds-Format.R

--- gds-Format.R
+++ gds-Format.R
@@ -3,4 +3,4 @@ library(gdsfmt)
 library("devtools")
 library(ggplot2)
 #### Make the .gds format. Depending on the size of the vcf file it can take some tome. Run an R script to generate the gds file. See example of R script `gds-Format.R` and to run the job see example `Job-gdsFormat.sh`.
-snpgdsVCF2GDS("newH-genolike1-greenjay-UNPLACED-10Kkb.vcf.recode.vcf", "genolike1-greenjay-UNPLACED-PRUNE.gds", method="biallelic.only")
+snpgdsVCF2GDS("newH-genolike1-greenjay_AUTOSOMES.NOmissing.THIN.recode.vcf", "newH-genolike1-greenjay_AUTOSOMES.NOmissing.THIN.gds", method="biallelic.only")
